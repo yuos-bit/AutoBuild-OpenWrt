3fe1890009b7421da9fc904db34454bb042e2825
小渔 <864529498@qq.com>
2025-02-18 23:36:47 +0800

    调整插件进行测试

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,7 +26,6 @@ sed -i '2a ifconfig rai0 up\nifconfig ra0 up\nbrctl addif br-lan rai0\nbrctl add
 # 单独拉取软件包
 git clone -b Lienol-default-settings https://github.com/yuos-bit/other package/default-settings
 
-git clone -b master https://github.com/yuos-bit/luci-theme-edge package/luci-theme-edge
-git clone -b master https://github.com/yuos-bit/luci-theme-netgear package/luci-theme-netgear
+git clone -b debug https://github.com/yuos-bit/luci-theme-edge package/luci-theme-argon
 # 更改默认wifi
 # cp -rf $GITHUB_WORKSPACE/patchs/NX30Pro/mtwifi.sh package/mtk/applications/mtwifi-cfg/files/mtwifi.sh

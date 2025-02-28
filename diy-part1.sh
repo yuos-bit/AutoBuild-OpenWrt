816482138a185ae1f6287bf780499546aa9512bd
小渔 <864529498@qq.com>
2025-02-28 11:41:44 +0800

    测试编译

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,6 @@
 # 增加软件包
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small.git;master' feeds.conf.default
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package.git;main' feeds.conf.default
 
 # 修改默认编译LUCI进系统

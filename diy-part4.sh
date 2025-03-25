2f4d581750d7dbf5f9c22e81076c4fbdb6337aae
小渔 <864529498@qq.com>
2025-03-25 13:33:22 +0800

    aadd

--- diy-part4.sh
+++ diy-part4.sh
@@ -19,31 +19,23 @@
 # cp -rf $GITHUB_WORKSPACE/patchs/4.14/ar71xx/* target/linux/ar71xx
 
 # 修改软件包版本为大杂烩-openwrt19.07
-# sed -i 's/git.openwrt.org\/feed\/packages.git;openwrt-19.07/github.com\/Lienol\/openwrt-packages.git;19.07/g' feeds.conf.default
-# sed -i 's/git.openwrt.org\/project\/luci.git;openwrt-19.07/github.com\/coolsnowwolf\/luci.git;master/g' feeds.conf.default
-# 
-# # 增加软件包
-# sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small.git;master' feeds.conf.default
-# sed -i '$a src-git small8 https://github.com/kenzok8/small-package.git;main' feeds.conf.default
-# 
-# 
-# # 修改默认dnsmasq为dnsmasq-full
-# sed -i 's/dnsmasq/dnsmasq-full luci/g' include/target.mk
-# 
-# # 修改默认编译LUCI进系统
-# sed -i 's/ppp-mod-pppoe/ppp-mod-pppoe default-settings luci curl/g' include/target.mk
-# 
-# # 单独拉取软件包
-# git clone -b Lienol-default-settings https://github.com/yuos-bit/other package/default-settings
-# git clone -b main --single-branch https://github.com/yuos-bit/other package/yuos
-# git clone -b debug https://github.com/yuos-bit/luci-theme-edge2 package/luci-theme-edge2
-# 补充包
-# 修改默认斐讯K2 wifi驱动为闭源驱动
-# sed -i 's/kmod-mt76x2/kmod-mt7612e luci-app-mtwifi/g' target/linux/ramips/image/mt7621.mk
+sed -i 's/git.openwrt.org\/feed\/packages.git;openwrt-19.07/github.com\/Lienol\/openwrt-packages.git;19.07/g' feeds.conf.default
+sed -i 's/git.openwrt.org\/project\/luci.git;openwrt-19.07/github.com\/coolsnowwolf\/luci.git;master/g' feeds.conf.default
 
-# 设置闭源驱动开机自启
-# sed -i '2a ifconfig rai0 up\nifconfig ra0 up\nbrctl addif br-lan rai0\nbrctl addif br-lan ra0' package/base-files/files/etc/rc.local
+# 增加软件包
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small.git;master' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package.git;main' feeds.conf.default
 
-# 添加4.14内核ACC、shortcut-fe补丁
+
+# 修改默认dnsmasq为dnsmasq-full
+sed -i 's/dnsmasq/dnsmasq-full luci/g' include/target.mk
+
+# 修改默认编译LUCI进系统
+sed -i 's/ppp-mod-pppoe/ppp-mod-pppoe default-settings luci curl/g' include/target.mk
+
+# 单独拉取软件包
+git clone -b Lienol-default-settings https://github.com/yuos-bit/other package/default-settings
+git clone -b main --single-branch https://github.com/yuos-bit/other package/yuos
+git clone -b debug https://github.com/yuos-bit/luci-theme-edge2 package/luci-theme-edge2
--- diy-part5.sh
+++ diy-part5.sh
@@ -10,57 +10,41 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #=================================================
 
-# 修改默认IP
-# sed -i 's/192.168.1.1/10.32.0.1/g' package/base-files/files/bin/config_generate
-# # 修改网关
-# sed -i 's/192.168.$((addr_offset++)).1/10.32.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
+修改默认IP
+sed -i 's/192.168.1.1/10.32.0.1/g' package/base-files/files/bin/config_generate
+# 修改网关
+sed -i 's/192.168.$((addr_offset++)).1/10.32.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
+
+# 修改主机名称
+sed -i 's/OpenWrt/Yuos/g' package/base-files/files/bin/config_generate
+
+# 修改默认wifi名称ssid为Xiaoyu-Wifi
+sed -i 's/ssid=OpenWrt/ssid=Xiaomi-Wifi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改默认wifi密码key为1234567890
+sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#使用sed 在第四行后添加新字
+sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=1234567890' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 
-# # 修改主机名称
-# sed -i 's/OpenWrt/Yuos/g' package/base-files/files/bin/config_generate
+# nft-fullcone
+git clone -b main --single-branch https://github.com/fullcone-nat-nftables/nftables-1.0.5-with-fullcone package/nftables
+git clone -b master --single-branch https://github.com/fullcone-nat-nftables/libnftnl-1.2.4-with-fullcone package/libnftnl
+
+# dnsmasq-full升级2.89
+rm -rf package/network/services/dnsmasq
+cp -rf $GITHUB_WORKSPACE/patchs/5.4/dnsmasq package/network/services/dnsmasq
 # 
-# # 修改默认wifi名称ssid为Xiaoyu-Wifi
-# sed -i 's/ssid=OpenWrt/ssid=Xiaomi-Wifi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# 
-# # 修改默认wifi密码key为1234567890
-# sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# 
-# #使用sed 在第四行后添加新字
-# sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=1234567890' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# 
-# 
-# # Add kernel build user
-# [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
-#     echo 'CONFIG_KERNEL_BUILD_USER="MOLUN"' >>.config ||
-#     sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"MOLUN"@' .config
-# 
-# # Add kernel build domain
-# [ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
-#     echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
-#     sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
-# 
-# 
-# # nft-fullcone
-# git clone -b main --single-branch https://github.com/fullcone-nat-nftables/nftables-1.0.5-with-fullcone package/nftables
-# git clone -b master --single-branch https://github.com/fullcone-nat-nftables/libnftnl-1.2.4-with-fullcone package/libnftnl
-# 
-# # 打补丁
-# cp -n $GITHUB_WORKSPACE/patchs/4.14/hack-4.14/* target/linux/generic/hack-4.14/
-# cp -n $GITHUB_WORKSPACE/patchs/4.14/backport-4.14/* target/linux/generic/backport-4.14/
-# cp -n $GITHUB_WORKSPACE/patchs/4.14/pending-4.14/* target/linux/generic/pending-4.14/
-# # dnsmasq-full升级2.89
-# rm -rf package/network/services/dnsmasq
-# cp -rf $GITHUB_WORKSPACE/patchs/5.4/dnsmasq package/network/services/dnsmasq
-# 
-# # 测试编译时间
-# YUOS_DATE="$(date +%Y.%m.%d)(养老版)"
-# BUILD_STRING=${BUILD_STRING:-$YUOS_DATE}
-# echo "Write build date in openwrt : $BUILD_DATE"
-# echo -e '\n小渔学长 Build @ '${BUILD_STRING}'\n'  >> package/base-files/files/etc/banner
-# sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
-# echo "DISTRIB_REVISION=''" >> package/base-files/files/etc/openwrt_release
-# sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-# echo "DISTRIB_DESCRIPTION='小渔学长 Build @ ${BUILD_STRING}'" >> package/base-files/files/etc/openwrt_release
-# sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
+# 测试编译时间
+YUOS_DATE="$(date +%Y.%m.%d)(养老版)"
+BUILD_STRING=${BUILD_STRING:-$YUOS_DATE}
+echo "Write build date in openwrt : $BUILD_DATE"
+echo -e '\n小渔学长 Build @ '${BUILD_STRING}'\n'  >> package/base-files/files/etc/banner
+sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_REVISION=''" >> package/base-files/files/etc/openwrt_release
+sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='小渔学长 Build @ ${BUILD_STRING}'" >> package/base-files/files/etc/openwrt_release
+sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
 # 
 #升级cmake
 find . -type d -name "cmake" -exec rm -r {} +

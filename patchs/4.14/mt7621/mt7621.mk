ca6d1b3d33bec185f82cff62e5738e9adb3273bd
小渔 <864529498@qq.com>
2025-03-28 12:03:53 +0800

    Update mt7621.mk

--- patchs/4.14/mt7621/mt7621.mk
+++ patchs/4.14/mt7621/mt7621.mk
@@ -685,8 +685,6 @@ TARGET_DEVICES += zbt-wg3526-32M
 
 define Device/e8820v2
   DTS := E8820V2
-  $(Device/dsa-migration)
-  $(Device/uimage-lzma-loader)
   IMAGE_SIZE := 16064k
   DEVICE_TITLE := ZTE E8820V2
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb3 \

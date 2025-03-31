5e67f792b7860642d451c1279485bcd65e16ddb6
小渔 <864529498@qq.com>
2025-03-31 14:10:33 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -4,18 +4,6 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# 复制小米路由配置文件到编译目录
-# cp -rf $GITHUB_WORKSPACE/patchs/4.14/dts/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/dts/
-# cp -rf $GITHUB_WORKSPACE/patchs/4.14/mt76x8/mt76x8.mk $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/mt76x8.mk
-# cp -rf $GITHUB_WORKSPACE/patchs/4.14/mt7621/mt7621.mk $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/mt7621.mk
-# cp -rf $GITHUB_WORKSPACE/patchs/4.14/public/01_leds $GITHUB_WORKSPACE/openwrt/target/linux/ramips/base-files/etc/board.d/01_leds
-# cp -rf $GITHUB_WORKSPACE/patchs/4.14/public/02_network $GITHUB_WORKSPACE/openwrt/target/linux/ramips/base-files/etc/board.d/02_network
-
 # 增加软件包
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default

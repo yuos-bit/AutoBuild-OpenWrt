4210a34e2d5dbafb7fe7dd2b8ef3691af349215b
小渔 <864529498@qq.com>
2025-06-10 21:13:07 +0800

    重新测试E8820V2

--- diy-part4.sh
+++ diy-part4.sh
@@ -12,3 +12,9 @@ sed -i '$a src-git small8 https://github.com/kenzok8/small-package.git;main' fee
 # 单独拉取软件包
 git clone -b 19.07 https://github.com/yuos-bit/other package/19.07
 git clone -b main --single-branch https://github.com/yuos-bit/other package/yuos
+# 覆盖源码
+cp -rf $GITHUB_WORKSPACE/patchs/4.14/dts/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/dts/
+cp -rf $GITHUB_WORKSPACE/patchs/4.14/mt76x8/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/
+cp -rf $GITHUB_WORKSPACE/patchs/4.14/mt7621/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/
+cp -rf $GITHUB_WORKSPACE/patchs/4.14/board.d/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/base-files/etc/board.d/
+cp -rf $GITHUB_WORKSPACE/patchs/4.14/wifi/mac80211.sh $GITHUB_WORKSPACE/openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh

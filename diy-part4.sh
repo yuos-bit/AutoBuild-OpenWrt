d6296742b1fe32e53ad997f80fa98be4f233d97a
小渔 <864529498@qq.com>
2025-06-13 09:00:09 +0800

    测试 default-settings 对网口的影响

--- diy-part4.sh
+++ diy-part4.sh
@@ -17,3 +17,9 @@ cp -rf $GITHUB_WORKSPACE/patchs/4.14/mt76x8/* $GITHUB_WORKSPACE/openwrt/target/l
 cp -rf $GITHUB_WORKSPACE/patchs/4.14/mt7621/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/
 cp -rf $GITHUB_WORKSPACE/patchs/4.14/board.d/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/base-files/etc/board.d/
 cp -rf $GITHUB_WORKSPACE/patchs/4.14/wifi/mac80211.sh $GITHUB_WORKSPACE/openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改默认dnsmasq为dnsmasq-full
+sed -i 's/dnsmasq-full/dnsmasq-full coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-ipt-raw6 kmod-tun luci-app-webadmin/g' include/target.mk
+
+# 修改默认编译LUCI进系统
+sed -i 's/ppp-mod-pppoe/iptables-mod-tproxy iptables-mod-extra ipset ip-full ppp ppp-mod-pppoe/g' include/target.mk

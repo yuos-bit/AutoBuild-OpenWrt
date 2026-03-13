13c4700af12e41e188ce21fbb98ae64de5584826
小渔 <864529498@qq.com>
2026-03-13 23:54:19 +0800

    Update openwrt19.07-2.sh

--- openwrt19.07-2.sh
+++ openwrt19.07-2.sh
@@ -22,9 +22,9 @@ git clone -b main --single-branch https://github.com/fullcone-nat-nftables/nftab
 git clone -b master --single-branch https://github.com/fullcone-nat-nftables/libnftnl-1.2.4-with-fullcone package/libnftnl
 
 # 最新主题
-find feeds/luci -type d -name "luci-theme-bootstrap" -exec rm -rf {} +
-rm -rf package/yuos/luci-theme-bootstrap
-git clone -b master https://github.com/eamonxg/luci-theme-bootstrap package/yuos
+# find feeds/luci -type d -name "luci-theme-bootstrap" -exec rm -rf {} +
+# rm -rf package/yuos/luci-theme-bootstrap
+# git clone -b master https://github.com/eamonxg/luci-theme-bootstrap package/yuos
 
 # 
 # 测试编译时间

f36152631fc311a2af12097cbe86a20e9bb45d97
小渔 <864529498@qq.com>
2026-06-03 20:59:13 +0800

    Update imwrt-mt7981-2.sh

--- imwrt-mt7981-2.sh
+++ imwrt-mt7981-2.sh
@@ -12,13 +12,6 @@
 
 # 修复libopenssl-legacy报错
 sed -i 's/ +libopenssl-legacy//g' package/passwall/shadowsocksr-libev/Makefile
-# 打补丁
-wget -O package/firmware/xt_FULLCONENAT.c https://raw.githubusercontent.com/Chion82/netfilter-full-cone-nat/master/xt_FULLCONENAT.c
-cp -rf package/firmware/xt_FULLCONENAT.c package/libs/libnetfilter-conntrack/xt_FULLCONENAT.c
-
-# nft-fullcone
-git clone -b main --single-branch https://github.com/fullcone-nat-nftables/nftables-1.0.5-with-fullcone package/nftables
-git clone -b master --single-branch https://github.com/fullcone-nat-nftables/libnftnl-1.2.4-with-fullcone package/libnftnl
 
 # 测试编译时间
 YUOS_DATE="$(date +%Y.%m.%d)(月更版)"
@@ -33,15 +26,7 @@ echo "DISTRIB_DESCRIPTION='小渔学长 Build @ ${BUILD_STRING}'" >> package/bas
 # 修改 luci version.lua
 sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
 echo "luciversion = '${BUILD_STRING}'" >> feeds/luci/modules/luci-base/luasrc/version.lua
-#FullCone Patch
-git clone -b master --single-branch https://github.com/QiuSimons/openwrt-fullconenat package/fullconenat
-# Patch FireWall for fullcone
-mkdir package/network/config/firewall/patches
-wget -P package/network/config/firewall/patches/ https://raw.githubusercontent.com/LGA1150/fullconenat-fw3-patch/master/fullconenat.patch
 
-pushd feeds/luci
-wget -O- https://raw.githubusercontent.com/LGA1150/fullconenat-fw3-patch/master/luci.patch | git apply
-popd
 
 #升级golang
 rm -rf feeds/packages/lang/golang
